74a10db80ed0c8830c5bbad62f25b055ffc91d64
Ksenia <kseniabelikova35@gmail.com>
2021-01-13 11:25:14 +0300

    import/export

---
+++ lesson4/task1/calculator/index.js
@@ -0,0 +1,14 @@
+export const sum = (a, b) => {
+  console.log("implementation for sum");
+  // ... implementation
+};
+
+export const mult = (a, b) => {
+  console.log("implementation for mult");
+  // ... implementation
+};
+
+export default (expression) => {
+  console.log("implementation for calc");
+  // ... implementation
+};
---
+++ lesson4/task1/polyfills/array-flatMap.js
@@ -0,0 +1,7 @@
+console.log("polyfill for flatMap");
+
+Array.prototype.flatMap =
+  Array.prototype.flatMap ||
+  function flatMap() {
+    // ... implementation for older browsers
+  };
