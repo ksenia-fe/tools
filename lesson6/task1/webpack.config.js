722a0543edf13d8fc522acd4f58fd45569b7e5e2
Ksenia <kseniabelikova35@gmail.com>
2021-01-15 16:08:01 +0300

    dev-server --

--- lesson6/task1/webpack.config.js
+++ lesson6/task1/webpack.config.js
@@ -1,38 +1,56 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /.s?css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /.(png|jpg)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
-              name: "[name].[ext]",
-              outputPath: "images",
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const config = {
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /.s?css$/,
+          use: [
+            "production" ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /.(png|jpg)$/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 8192,
+                name: "[name].[ext]",
+                outputPath: "images",
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-  ],
+    devServer: {
+      port: 9000,
+      hot: true,
+    },
+  };
+
+  if (isProduction) {
+    plugins.config.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
+    );
+  }
+
+  return config;
 };
